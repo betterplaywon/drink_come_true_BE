589e41ffd62eb5e4c82325b7008986f2e0dc90fd
betterplaywon <kkgi1234@gmail.com>
2023-01-03 15:59:54 +0900

    🎨게시글 정렬과 데이터 구조 변경

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,118 @@
 const express = require("express");
 const router = express.Router();
+const { Post, Image, Comment, User } = require("../models");
 
-router.post("/", (req, res) => {
-  res.json([{ id: 1, content: "위스키몇병" }]);
+router.post("/", async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id,
+    });
+
+    const allPost = await Post.findOne({
+      where: { id: post.id },
+      include: [
+        {
+          model: Image,
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User, // 댓글 작성자
+              attributes: ["id", "nickname"],
+            },
+          ],
+        },
+        {
+          model: User, // 게시글 작성자
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: User, // 게시글 작성자
+          as: "Likers",
+          attributes: ["id"],
+        },
+      ],
+    });
+
+    res.status(201).json(allPost);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
-router.delete("/", (req, res) => {
-  res.json([{ id: 1 }]);
+router.post("/:postId/comment", async (req, res, next) => {
+  try {
+    const isPost = Post.findOne({
+      where: { id: req.params.postId },
+    });
+    if (!isPost) {
+      return res.status(403).send("삭제되었거나 없는 게시글입니다");
+    }
+
+    const comment = await Comment.create({
+      content: req.body.content,
+      PostId: req.params.postId,
+      UserId: req.user.id,
+    });
+
+    const allComment = await Comment.findOne({
+      where: { id: comment.id },
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+      ],
+    });
+
+    res.status(201).json(allComment);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.patch("/:postId/like", async (req, res, next) => {
+  try {
+    const post = await Post.findOne({ where: { id: req.params.postId } });
+    if (!post) {
+      return res.status(403).send("존재하지 않는 게시글입니다.");
+    }
+    await post.addLikers(req.user.id);
+    res.json({ PostId: post.id, UserId: req.user.id });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete("/:postId/like", async (req, res, next) => {
+  try {
+    const post = await Post.findOne({ where: { id: req.params.postId } });
+    if (!post) {
+      return res.status(403).send("존재하지 않는 게시글입니다.");
+    }
+    await post.removeLikers(req.user.id);
+    res.json({ PostId: post.id, UserId: req.user.id });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete("/:postId", async (req, res, next) => {
+  try {
+    await Post.destroy({
+      where: { id: req.params.postId, UserId: req.user.id },
+    });
+    res.status(200).json({ PostId: parseInt(req.params.postId, 10) });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
